db45f3e98225b5c602f42ddb3b936ff318219f0f
Evyatar Haim <evyataros1@gmail.com>
2020-11-24 13:30:36 +0200

    creating a Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <Div>
+            I'm a Header
+        </Div>
+    )
+}
+
+export default Header;
+
+const Div = styled.div``
