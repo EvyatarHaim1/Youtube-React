92650a39143393b25be5a96db0cfc936bddd95aa
Evyatar Haim <evyataros1@gmail.com>
2020-11-24 17:32:09 +0200

    finish the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,19 +10,27 @@ import Avatar from '@material-ui/core/Avatar';
 function Header() {
     return (
         <Div>
-            <MenuIcon />
-            <img className="header_logo"
-                 src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" 
-                 alt="youtubeIcon" />
-            <input type="text" />
-            <SearchIcon />
-            <VideoCallIcon />
-            <AppsIcon />
-            <NotificationsIcon />
-            <Avatar
+            <div className="headerLeft">
+                <MenuIcon />
+                <img className="header_logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" 
+                    alt="youtubeIcon" />
+            </div>
+            
+            <div className="headerInput">
+                <input type="text" placeholder="Search" />
+                <SearchIcon className="headerInputBtn" />
+            </div>
+
+            <div className="headerIcons">
+            <VideoCallIcon className="headerIcon" />
+            <AppsIcon className="headerIcon" />
+            <NotificationsIcon className="headerIcon" />
+            <Avatar className="headerIcon"
                     alt="Evyatar Haim"
                     src="https://scontent.ftlv15-1.fna.fbcdn.net/v/t1.0-9/81016903_10157017848126947_5976747459706093568_o.jpg?_nc_cat=111&ccb=2&_nc_sid=84a396&_nc_ohc=IpkijUXYQY4AX-mKBJM&_nc_ht=scontent.ftlv15-1.fna&oh=d617c1becd9eaaaf3e66d1ad99f88e35&oe=5FE0FD6D"
                      />
+            </div>
         </Div>
     )
 }
@@ -32,8 +40,39 @@ export default Header;
 const Div = styled.div`
 display: flex;
 align-items: center;
+justify-content: space-between;
+padding: 20px;
+position: sticky;
+top: 0;
+z-index: 100;
+background-color: white;
 
 .header_logo{
    height: 25px;
    object-fit: contain;
-}`
+   margin-left: 20px;
+}
+.headerInput{
+    display: flex;
+    align-items: center;
+    width: 40%;
+    border: 1px solid lightgray;
+    input{
+        flex: 1;
+        border: none;
+    }
+    .headerInputBtn{
+        width: 50px !important;
+        background-color: #fafafa;
+        border-left: 1px solid lightgray;
+        color: gray;
+    }
+}
+.headerIcons{
+    display: flex;
+    align-items:center;
+}
+.headerIcon{
+    margin-right: 8px;
+}
+`
